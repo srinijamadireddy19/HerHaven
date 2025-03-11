4419d5b4a57ddf797d39f02155ece5039d608233
srinijamadireddy19 <srinijamadireddy111@gmail.com>
2025-03-11 11:25:10 +0530

    errors resolved

--- herhaven-client/src/context/AuthContext.jsx
+++ herhaven-client/src/context/AuthContext.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createContext, useContext, useReducer } from "react"
 import React from 'react'
 import authReducer from "../reducer/authReducer";
@@ -21,28 +20,4 @@ export  const AuthProvider = ({children}) =>  {
   )
 }
 
-=======
-import { createContext, useContext, useReducer } from "react"
-import React from 'react'
-import authReducer from "../reducer/authReducer";
-
-
-export const AuthContext = createContext();
-
-export  const AuthProvider = ({children}) =>  {
-    const initialState = { isAuthenticated: false, user: null };
-    const [state, dispatch] = useReducer(authReducer, initialState);
-
-    const login = (user) => dispatch({ type: "LOGIN", payload: user });
-    const updateProfile = (user) => dispatch({ type: "UPDATE", payload: user });
-    const logout = () => dispatch({ type: "LOGOUT" });
-
-  return (
-    <AuthContext.Provider value={{ state, login, logout, updateProfile }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
-
->>>>>>> a7a5ac24d79e247fd1326e8fd049e3b6ab827e35
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext); 
